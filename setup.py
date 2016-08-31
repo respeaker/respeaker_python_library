596467d1e26d309cc65aead435b4ee525a4df699
Yihui Xiong <yihui_xiong@163.com>
2016-08-31 14:35:22 +0800

    fix package data copy bug

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ setup(
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
     package_data={
-        'respeaker': ['pocketsphinx-data/*', 'pocketsphinx-data/hmm/*'],
+        'respeaker': ['pocketsphinx-data/en.dic', 'pocketsphinx-data/keywords.txt', 'pocketsphinx-data/hmm/*'],
     },
 
     # To provide executable scripts, use entry points in preference to the
