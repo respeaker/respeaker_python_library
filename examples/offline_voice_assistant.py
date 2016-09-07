c94d206fdd68ba07f2b701bb181f14e4fbdedfa9
Yihui Xiong <yihui_xiong@163.com>
2016-09-07 22:13:18 +0800

    add light

--- examples/offline_voice_assistant.py
+++ examples/offline_voice_assistant.py
@@ -20,34 +20,50 @@ from threading import Thread, Event
 
 import pyaudio
 
-from respeaker import Microphone
-from respeaker import Player
+try:
+    from respeaker import *
+except ImportError:
+    import fix_import
+    from respeaker import *
+
 
 mic = None
-quit_event = Event()
 
 
-def main():
-    global mic, quit_event
+def task(quit_event):
+    global mic
+
+    pixels = PixelRing()
+    pixels.set_color(rgb=0x400000)
 
     pa = pyaudio.PyAudio()
     mic = Microphone(pa)
-    player = Player(pa)
+
+    pixels.set_color(rgb=0x004000)
+    time.sleep(2)
+    pixels.off()
 
     while not quit_event.is_set():
-        if mic.detect(keyword='hey respeaker'):
-            print('wakeup')
-            command = mic.detect(max_phrase_ms=6000, max_wait_ms=4000)
-            if command:
-                print('recognized: ' + command)
-                if command.find('play music') > 0:
-                    pass
+        if mic.wakeup(keyword='alexa'):
+            print('Wake up')
+            pixels.listen()
 
+            data = mic.listen()
+            text = mic.recognize(data)
+
+            pixels.wait()
+            if text.find('play music') >= 0:
+                print('Play music')
+
+        pixels.off()
+
+    pixels.off()
     mic.close()
 
 
-if __name__ == '__main__':
-    thread = Thread(target=main)
+def main():
+    quit_event = Event()
+    thread = Thread(target=task, args=(quit_event,))
     thread.start()
     while True:
         try:
@@ -55,7 +71,11 @@ if __name__ == '__main__':
         except KeyboardInterrupt:
             print('\nquit')
             quit_event.set()
-            mic.interrupt(True, True)
+            mic.quit()
             break
 
     thread.join()
+
+
+if __name__ == '__main__':
+    main()
