f3f696b8fc26e83f3ed0057eb21ab7ab2a6931c6
Yihui Xiong <yihui_xiong@163.com>
2016-10-18 16:18:16 +0800

    rewrite

--- respeaker/microphone.py
+++ respeaker/microphone.py
@@ -15,28 +15,24 @@
  limitations under the License.
 """
 
-import Queue
-import collections
+
 import os
-import sys
 import wave
 import types
-import pyaudio
-import webrtcvad
+import Queue
+import collections
 import random
 import string
+import logging
 from threading import Thread, Event
-from pixel_ring import PixelRing
+
+import pyaudio
+
+from pixel_ring import pixel_ring
+from vad import vad
 
 
-ACTIVE_CHECK_CHUNKS = [6, 8]  # switch to active state if 8 chunks contains at least 4 active chunks
-IDLE_CHECK_CHUNKS = [1, 64]  # switch to idle state if 64 chunks contains less than 2 active chunks
-
-RING_SIZE = 2 ** 7
-RING_MASK = RING_SIZE - 1
-
-current_file_dir = os.path.dirname(os.path.realpath(__file__))
-pocketsphinx_data = os.getenv('POCKETSPHINX_DATA', os.path.join(current_file_dir, 'pocketsphinx-data'))
+logger = logger = logging.getLogger('mic')
 
 
 def random_string(length):
@@ -56,20 +52,18 @@ def save_as_wav(data, prefix):
     f.writeframes(data)
     f.close()
 
-    print('Save audio as %s' % filename)
+    logger.info('Save audio as %s' % filename)
 
 
 class Microphone:
     sample_rate = 16000
-    bytes_30ms = 30 * 2 * sample_rate / 1000  # 30 ms frames bytes
-
+    frames_per_buffer = 512
     listening_mask = (1 << 0)
     detecting_mask = (1 << 1)
-    recording_mask = (1 << 7)
+    recording_mask = (1 << 2)
 
-    def __init__(self, pyaudio_instance=None, quit_event=None, use_pocketsphinx=True):
-        self.ring = PixelRing()
-        self.ring.set_color(rgb=0x400000)
+    def __init__(self, pyaudio_instance=None, quit_event=None):
+        pixel_ring.set_color(rgb=0x400000)
     
         self.pyaudio_instance = pyaudio_instance if pyaudio_instance else pyaudio.PyAudio()
 
@@ -79,7 +73,7 @@ class Microphone:
                 name = dev['name'].encode('utf-8')
                 # print(i, name, dev['maxInputChannels'], dev['maxOutputChannels'])
                 if name.lower().find('respeaker') >= 0 and dev['maxInputChannels'] > 0:
-                    print('Use {}'.format(name))
+                    logger.info('Use {}'.format(name))
                     self.device_index = i
                     break
 
@@ -89,82 +83,66 @@ class Microphone:
             format=pyaudio.paInt16,
             channels=1,
             rate=self.sample_rate,
-            frames_per_buffer=2048,
+            frames_per_buffer=self.frames_per_buffer,
             stream_callback=self._callback,
             input_device_index=self.device_index,
         )
 
         self.quit_event = quit_event if quit_event else Event()
 
-        self.queue = Queue.Queue()
-        self.vad = webrtcvad.Vad(0)
+        self.listen_queue = Queue.Queue()
+        self.detect_queue = Queue.Queue()
 
-        self.decoder = None
-        if use_pocketsphinx:
-            self.decoder = self.get_decoder()
+        self.decoder = self.create_decoder()
+        self.decoder.start_utt()
 
         self.status = 0
-
         self.active = False
 
-        self.data_ring_buffer = collections.deque(maxlen=12)
-        self.flag_ring_buffer = bytearray(RING_SIZE)
-        self.flag_ring_index = 0
-
-        self.collect_ring_buffer = collections.deque(maxlen=48)
-
-        self.data = ''
+        self.listen_history = collections.deque(maxlen=8)
+        self.detect_history = collections.deque(maxlen=48)
 
         self.wav = None
-        self.recording_countdown = None
+        self.record_countdown = None
+        self.listen_countdown = [0, 0]
 
     @staticmethod
     def create_decoder():
         from pocketsphinx.pocketsphinx import Decoder
 
+        path = os.path.dirname(os.path.realpath(__file__))
+        pocketsphinx_data = os.getenv('POCKETSPHINX_DATA', os.path.join(path, 'pocketsphinx-data'))
+        hmm = os.getenv('POCKETSPHINX_HMM', os.path.join(pocketsphinx_data, 'hmm'))
+        dict = os.getenv('POCKETSPHINX_DIC', os.path.join(pocketsphinx_data, 'dictionary.txt'))
+        kws = os.getenv('POCKETSPHINX_KWS', os.path.join(pocketsphinx_data, 'keywords.txt'))
+
         config = Decoder.default_config()
-        config.set_string('-hmm', os.path.join(pocketsphinx_data, 'hmm'))
-        config.set_string('-dict', os.path.join(pocketsphinx_data, 'dictionary.txt'))
-        config.set_string('-kws', os.path.join(pocketsphinx_data, 'keywords.txt'))
+        config.set_string('-hmm', hmm)
+        config.set_string('-dict', dict)
+        config.set_string('-kws', kws)
         # config.set_int('-samprate', SAMPLE_RATE) # uncomment if rate is not 16000. use config.set_float() on ubuntu
-        config.set_int('-nfft', 2048)
+        config.set_int('-nfft', 512)
+        config.set_float('-vad_threshold', 2.7)
         config.set_string('-logfn', os.devnull)
 
         return Decoder(config)
 
-    def get_decoder(self):
-        if not self.decoder:
-            self.decoder = self.create_decoder()
-            self.decoder.start_utt()
-        return self.decoder
-
-    def get_pyaudio(self):
-        return self.pyaudio_instance
-
     def recognize(self, data):
-        if not self.decoder:
-            self.decoder = self.create_decoder()
-            self.decoder.start_utt()
-        else:
-            self.decoder.end_utt()
-            self.decoder.start_utt()
+        self.decoder.end_utt()
+        self.decoder.start_utt()
 
         if not data:
             return ''
 
         if isinstance(data, types.GeneratorType):
-            audio = ''
             for d in data:
                 self.decoder.process_raw(d, False, False)
-                audio += d
         else:
             self.decoder.process_raw(data, False, True)
-            audio = data
 
         hypothesis = self.decoder.hyp()
         if hypothesis:
-            save_as_wav(audio, hypothesis.hypstr)
-            print('\nRecognized %s' % hypothesis.hypstr)
+            logger.info('Recognized {}'.format(hypothesis.hypstr))
             return hypothesis.hypstr
 
         return ''
@@ -173,92 +151,88 @@ class Microphone:
         self.decoder.end_utt()
         self.decoder.start_utt()
 
-        self.ring.off()
+        pixel_ring.off()
 
-        self.collect_ring_buffer.clear()
+        self.detect_history.clear()
 
-        self.queue.queue.clear()
-        self.status |= self.listening_mask
+        self.detect_queue.queue.clear()
+        self.status |= self.detecting_mask
         self.stream.start_stream()
 
         result = None
+        logger.info('Start detecting')
         while not self.quit_event.is_set():
-            data = self.queue.get()
-            left = self.queue.qsize()
-            if left > 4:
-                print('%s chunks delay' % left)
+            size = self.detect_queue.qsize()
+            if size > 4:
+                logger.info('Too many delays, {} in queue'.format(size))
 
-            self.collect_ring_buffer.append(data)
-
+            data = self.detect_queue.get()
+            self.detect_history.append(data)
             self.decoder.process_raw(data, False, False)
+
             hypothesis = self.decoder.hyp()
             if hypothesis:
-                print('\nDetected %s' % hypothesis.hypstr)
-                save_as_wav(b''.join(self.collect_ring_buffer), hypothesis.hypstr)
-                self.collect_ring_buffer.clear()
+                logger.info('Detected {}'.format(hypothesis.hypstr))
+                save_as_wav(b''.join(self.detect_history), hypothesis.hypstr)
+                self.detect_history.clear()
                 if keyword:
                     if hypothesis.hypstr.find(keyword) >= 0:
-                        result = True
+                        result = hypothesis.hypstr
                         break
                     else:
                         self.decoder.end_utt()
                         self.decoder.start_utt()
-                        self.collect_ring_buffer.clear()
+                        self.detect_history.clear()
                 else:
                     result = hypothesis.hypstr
                     break
 
-        self.status &= ~self.listening_mask
+        self.status &= ~self.detecting_mask
         self.stop()
 
         return result
 
     wakeup = detect
 
-    def listen(self, duration=9, timeout=6, vad=True):
-        self.queue.queue.clear()
-        if vad:
-            self.data = ''
-            self.active = False
-            self.data_ring_buffer.clear()
-            self.flag_ring_buffer = bytearray(RING_SIZE)
-            self.status |= self.detecting_mask
-        else:
-            self.status |= self.listening_mask
+    def listen(self, duration=9, timeout=3):
+        vad.reset()
+
+        self.listen_countdown[0] = (duration * self.sample_rate + self.frames_per_buffer - 1) / self.frames_per_buffer
+        self.listen_countdown[1] = (timeout * self.sample_rate + self.frames_per_buffer - 1) / self.frames_per_buffer
+
+        self.listen_queue.queue.clear()
+        self.status |= self.listening_mask
         self.start()
-        self.ring.listen()
-        return self._listen(duration, timeout)
+        pixel_ring.listen()
 
-    def _listen(self, timeout, duration):
-        try:
-            phrase = 0.0
-            data = self.queue.get(timeout=timeout)
-            while data and not self.quit_event.is_set():
-                yield data
-                phrase += len(data) / 2.0 / self.sample_rate
-                if phrase >= duration:
-                    break
-                data = self.queue.get(timeout=timeout)
-        except Queue.Empty:
-            pass
+        logger.info('Start listening')
 
-        self.status &= ~(self.listening_mask & self.detecting_mask)
-        self.stop()
-        self.ring.wait()
+        def _listen():
+            try:
+                data = self.listen_queue.get(timeout=timeout)
+                while data and not self.quit_event.is_set():
+                    yield data
+                    data = self.listen_queue.get(timeout=timeout)
+            except Queue.Empty:
+                pass
 
-    def record(self, file_name, ms=1800000):
+            self.stop()
+
+        return _listen()
+
+    def record(self, file_name, seconds=1800):
         self.wav = wave.open(file_name, 'wb')
         self.wav.setsampwidth(2)
         self.wav.setnchannels(1)
         self.wav.setframerate(self.sample_rate)
-        self.recording_countdown = ms
+        self.record_countdown = (seconds * self.sample_rate + self.frames_per_buffer - 1) / self.frames_per_buffer
         self.status |= self.recording_mask
         self.start()
 
     def quit(self):
         self.status = 0
         self.quit_event.set()
-        self.queue.put('')
+        self.listen_queue.put('')
         if self.wav:
             self.wav.close()
             self.wav = None
@@ -268,7 +242,7 @@ class Microphone:
             self.stream.start_stream()
 
     def stop(self):
-        if not self.status and self.stream.is_active:
+        if not self.status and self.stream.is_active():
             self.stream.stop_stream()
 
     def close(self):
@@ -279,50 +253,41 @@ class Microphone:
         if self.status & self.recording_mask:
             pass
 
+        if self.status & self.detecting_mask:
+            self.detect_queue.put(in_data)
+
         if self.status & self.listening_mask:
-            self.queue.put(in_data)
-        elif self.status & self.detecting_mask:
-            self.data += in_data
-            while len(self.data) >= self.bytes_30ms:
-                data = self.data[:self.bytes_30ms]
-                self.data = self.data[self.bytes_30ms:]
-
-                self.data_ring_buffer.append(data)
-
-                active = self.vad.is_speech(data, self.sample_rate)
-                sys.stdout.write('1' if active else '0')
-                self.flag_ring_buffer[self.flag_ring_index] = 1 if active else 0
-                self.flag_ring_index += 1
-                self.flag_ring_index &= RING_MASK
+            active = vad.is_speech(in_data)
+            if active:
                 if not self.active:
-                    num_voiced = 0
-                    for i in range(0, ACTIVE_CHECK_CHUNKS[1]):
-                        num_voiced += self.flag_ring_buffer[self.flag_ring_index - i]
+                    for d in self.listen_history:
+                        self.listen_queue.put(d)
+                        self.listen_countdown[0] -= 1
 
-                    if num_voiced >= ACTIVE_CHECK_CHUNKS[0]:
-                        sys.stdout.write('+')
-                        self.active = True
-                        self.queue.put(b''.join(self.data_ring_buffer))
+                self.listen_queue.put(in_data)
+                self.listen_countdown[0] -= 1
+            else:
+                if self.active:
+                    self.listen_queue.put(in_data)
                 else:
-                    num_voiced = 0
-                    for i in range(0, IDLE_CHECK_CHUNKS[1]):
-                        num_voiced += self.flag_ring_buffer[self.flag_ring_index - i]
+                    self.listen_history.append(in_data)
 
-                    if num_voiced < IDLE_CHECK_CHUNKS[0]:
-                        sys.stdout.write('-')
-                        self.queue.put('')
-                        self.active = False
-                        self.status &= ~self.detecting_mask
-                        break
-                    else:
-                        self.queue.put(data)
+                self.listen_countdown[1] -= 1
 
-                sys.stdout.flush()
+            if self.listen_countdown[0] <= 0 or self.listen_countdown[1] <= 0:
+                self.listen_queue.put('')
+                self.status &= ~self.listening_mask
+                pixel_ring.wait()
+                logger.info('Stop listening')
+
+            self.active = active
 
         return None, pyaudio.paContinue
 
 
 def task(quit_event):
+    import time
+
     mic = Microphone(quit_event=quit_event)
 
     while not quit_event.is_set():
@@ -331,11 +296,14 @@ def task(quit_event):
             data = mic.listen()
             text = mic.recognize(data)
             if text:
+                time.sleep(3)
                 print('Recognized %s' % text)
 
 
 def main():
     import time
+
+    logging.basicConfig(level=logging.DEBUG)
 
     q = Event()
     t = Thread(target=task, args=(q,))
