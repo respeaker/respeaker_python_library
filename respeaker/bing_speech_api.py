6fbefd313b16e29028f03967f392a0d4522b9d02
Yihui Xiong <yihui_xiong@163.com>
2016-11-01 16:57:50 +0800

    use requests library instead of urllib

--- respeaker/bing_speech_api.py
+++ respeaker/bing_speech_api.py
@@ -1,18 +1,15 @@
-'''
+"""
 Bing Speech To Text (STT) and Text To Speech (TTS)
+"""
 
-based on https://github.com/Uberi/speech_recognition
-'''
-
-import json
+import io
+import types
 import uuid
 import wave
-import io
-from monotonic import monotonic
-from urllib import urlencode
-from urllib2 import Request, urlopen, URLError, HTTPError
+
 import requests
-import types
+from monotonic import monotonic
+
 
 class RequestError(Exception):
     pass
@@ -63,39 +60,39 @@ class BingSpeechAPI:
                       "Male": "Microsoft Server Speech Text to Speech Voice (zh-TW, Zhiwei, Apollo)"}
         }
 
-    def auth(self):
+        self.session = requests.Session()
+
+    def authenticate(self):
         if self.expire_time is None or monotonic() > self.expire_time:  # first credential request, or the access token from the previous one expired
             # get an access token using OAuth
             credential_url = "https://oxford-speech.cloudapp.net/token/issueToken"
-            credential_request = Request(credential_url, data=urlencode({
+            data = {
                 "grant_type": "client_credentials",
                 "client_id": "python",
                 "client_secret": self.key,
                 "scope": "https://speech.platform.bing.com"
-            }).encode("utf-8"))
+            }
             start_time = monotonic()
-            try:
-                credential_response = urlopen(credential_request)
-            except HTTPError as e:
-                raise RequestError("recognition request failed: {0}".format(
-                    getattr(e, "reason", "status {0}".format(e.code))))  # use getattr to be compatible with Python 2.6
-            except URLError as e:
-                raise RequestError("recognition connection failed: {0}".format(e.reason))
-            credential_text = credential_response.read().decode("utf-8")
-            credentials = json.loads(credential_text)
+            response = self.session.post(credential_url, data=data)
+
+            if response.status_code != 200:
+                raise RequestError("recognition connection failed")
+
+            credentials = response.json()
+
             self.access_token, expiry_seconds = credentials["access_token"], float(credentials["expires_in"])
 
             self.expire_time = start_time + expiry_seconds
 
-    def get_chunk_generator(self, audio):
-        yield self.get_wav_header()
-        for a in audio:
-            yield a
-
     def recognize(self, audio_data, language="en-US", show_all=False):
-        self.auth()
+        self.authenticate()
         if isinstance(audio_data, types.GeneratorType):
-            data = self.get_chunk_generator(audio_data)
+            def generate(audio):
+                yield self.get_wav_header()
+                for a in audio:
+                    yield a
+
+            data = generate(audio_data)
         else:
             data = self.to_wav(audio_data)
 
@@ -116,27 +113,13 @@ class BingSpeechAPI:
             "Content-Type": "audio/wav; samplerate=16000; sourcerate=16000; trustsourcerate=true",
         }
 
-        if False:
-            url = "https://speech.platform.bing.com/recognize/query?{0}".format(urlencode(params))
-            request = Request(url, headers=headers, data=data)
-            try:
-                response = urlopen(request)
-            except HTTPError as e:
-                raise RequestError("recognition request failed: {0}".format(
-                    getattr(e, "reason", "status {0}".format(e.code))))  # use getattr to be compatible with Python 2.6
-            except URLError as e:
-                raise RequestError("recognition connection failed: {0}".format(e.reason))
+        url = "https://speech.platform.bing.com/recognize/query"
+        response = self.session.post(url, params=params, headers=headers, data=data)
 
-            response_text = response.read().decode("utf-8")
-            result = json.loads(response_text)
-        else:
-            url = "https://speech.platform.bing.com/recognize/query"
-            response = requests.post(url, params=params, headers=headers, data=data)
+        if response.status_code != 200:
+            raise RequestError("recognition connection failed")
 
-            if response.status_code != 200:
-                raise RequestError("recognition connection failed")
-
-            result = response.json()
+        result = response.json()
 
         if show_all:
             return result
@@ -145,7 +128,7 @@ class BingSpeechAPI:
         return result["header"]["lexical"]
 
     def synthesize(self, text, language="en-US", gender="Female"):
-        self.auth()
+        self.authenticate()
 
         if language not in self.locales.keys():
             raise LocaleError("language locale not supported.")
@@ -174,20 +157,8 @@ class BingSpeechAPI:
         }
 
         url = "https://speech.platform.bing.com/synthesize"
-        if False:
-            request = Request(url, data=body, headers=headers)
-            try:
-                response = urlopen(request)
-            except HTTPError as e:
-                raise RequestError("tts request failed: {0}".format(
-                    getattr(e, "reason", "status {0}".format(e.code))))  # use getattr to be compatible with Python 2.6
-            except URLError as e:
-                raise RequestError("tts connection failed: {0}".format(e.reason))
-
-            data = response.read()
-        else:
-            r = requests.post(url, headers=headers, data=body)
-            data = r.content
+        response = self.session.post(url, headers=headers, data=body)
+        data = response.content
 
         return data
 
@@ -222,7 +193,7 @@ class BingSpeechAPI:
         return header
 
 
-if __name__ == '__main__':
+def main():
     import sys
 
     # get a key from https://www.microsoft.com/cognitive-services/en-us/speech-api
@@ -250,7 +221,9 @@ if __name__ == '__main__':
     try:
         text = bing.recognize(frames, language='en-US')
         print('Bing:' + text.encode('utf-8'))
-    except UnknownValueError:
-        print("Microsoft Bing Voice Recognition could not understand audio")
     except RequestError as e:
         print("Could not request results from Microsoft Bing Voice Recognition service; {0}".format(e))
+
+
+if __name__ == '__main__':
+    main()
