f4319b3bab0508d0bfde2702e91410c76cd59664
Yihui Xiong <yihui_xiong@163.com>
2016-12-05 17:12:45 +0800

    rewritten, with spectrum feature

--- respeaker/player.py
+++ respeaker/player.py
@@ -15,78 +15,116 @@
  limitations under the License.
 """
 
+import Queue
+import audioop
 import threading
-import time
+import types
 import wave
 
 import pyaudio
 
-CHUNK_SIZE = 4096
+from spectrum_analyzer import SpectrumAnalyzer
+from spi import spi
+
+CHUNK_SIZE = 1024
+BAND_NUMBER = 16
 
 
-class Player():
-    def __init__(self, pa):
-        self.pa = pa
-        self.event = threading.Event()
-        # self.stream = self.pa.open(format=pyaudio.paInt16,
-        #                            channels=1,
-        #                            rate=16000,
-        #                            output=True,
-        #                            start=False,
-        #                            # output_device_index=1,
-        #                            frames_per_buffer=CHUNK_SIZE,
-        #                            stream_callback=self.callback)
+class Player:
+    def __init__(self, pyaudio_instance=None):
+        self.pyaudio_instance = pyaudio_instance if pyaudio_instance else pyaudio.PyAudio()
+        self.stop_event = threading.Event()
 
-    def play(self, wav_file, block=True):
-        self.wav = wave.open(wav_file, 'rb')
-        self.event.clear()
-        self.stream = self.pa.open(format=self.pa.get_format_from_width(self.wav.getsampwidth()),
-                                   channels=self.wav.getnchannels(),
-                                   rate=self.wav.getframerate(),
-                                   output=True,
-                                   # output_device_index=1,
-                                   frames_per_buffer=CHUNK_SIZE,
-                                   stream_callback=self.wav_callback)
+        def ignite(queue):
+            data = queue.get()
+            analyzer = SpectrumAnalyzer(len(data), band_number=BAND_NUMBER)
+            while True:
+                while not queue.empty():
+                    data = queue.get()
+
+                amplitude = analyzer.analyze(data)
+                level = bytearray(len(amplitude))
+                for i, v in enumerate(amplitude):
+                    l = int(v / 1024 / 128)
+                    if l > 255:
+                        l = 255
+                    level[i] = l
+
+                spi.write(address=0xA0, data=level)
+
+                data = queue.get()
+
+        self.queue = Queue.Queue()
+        self.thread = threading.Thread(target=ignite, args=(self.queue,))
+        self.thread.daemon = True
+        self.thread.start()
+
+    def _play(self, data, rate=16000, channels=1, width=2, spectrum=True):
+        stream = self.pyaudio_instance.open(
+            format=self.pyaudio_instance.get_format_from_width(width),
+            channels=channels,
+            rate=rate,
+            output=True,
+            # output_device_index=1,
+            frames_per_buffer=CHUNK_SIZE,
+        )
+
+        if isinstance(data, types.GeneratorType):
+            for d in data:
+                if self.stop_event.is_set():
+                    break
+
+                stream.write(d)
+
+                if spectrum:
+                    if channels == 2:
+                        d = audioop.tomono(d, 2, 0.5, 0.5)
+                    self.queue.put(d)
+        else:
+            stream.write(data)
+
+        stream.close()
+
+    def play(self, wav=None, data=None, rate=16000, channels=1, width=2, block=True, spectrum=None):
+        if wav:
+            f = wave.open(wav, 'rb')
+            rate = f.getframerate()
+            channels = f.getnchannels()
+            width = f.getsampwidth()
+
+            def gen(w):
+                d = w.readframes(CHUNK_SIZE)
+                while d:
+                    yield d
+                    d = w.readframes(CHUNK_SIZE)
+                d.close()
+
+            data = gen(f)
+
+        self.stop_event.clear()
         if block:
-            self.event.wait()
-            time.sleep(2)             # wait for playing audio data in buffer, a alsa driver bug
-            self.stream.close()
+            self._play(data, rate, channels, width, spectrum)
+        else:
+            thread = threading.Thread(target=self._play, args=(data, rate, channels, width, spectrum))
+            thread.start()
 
-    def play_raw(self, raw_data, rate=16000, channels=1, width=2, block=True):
-        self.raw = raw_data
-        self.width = width
-        self.channels = channels
-        self.event.clear()
-        self.stream = self.pa.open(format=self.pa.get_format_from_width(width),
-                                   channels=channels,
-                                   rate=rate,
-                                   output=True,
-                                   # output_device_index=1,
-                                   frames_per_buffer=CHUNK_SIZE,
-                                   stream_callback=self.raw_callback)
-        if block:
-            self.event.wait()
-            time.sleep(2)             # wait for playing audio data in buffer, a alsa driver bug
-            self.stream.close()
+    def play_raw(self, data, rate=16000, channels=1, width=2):
+        self.play(data=data, rate=rate, channels=channels, width=width)
 
-    def wav_callback(self, in_data, frame_count, time_info, status):
-        data = self.wav.readframes(frame_count)
-        flag = pyaudio.paContinue
-        if self.wav.getnframes() == self.wav.tell():
-            data = data.ljust(frame_count * self.wav.getsampwidth() * self.wav.getnchannels(), '\x00')
-            # flag = pyaudio.paComplete
-            self.event.set()
+    def stop(self):
+        self.stop_event.set()
 
-        return data, flag
 
-    def raw_callback(self, in_data, frame_count, time_info, status):
-        size = frame_count * self.width * self.channels
-        data = self.raw[:size]
-        self.raw = self.raw[size:]
-        flag = pyaudio.paContinue
-        if not len(self.raw):
-            data = data.ljust(frame_count * self.width * self.channels, '\x00')
-            # flag = pyaudio.paComplete
-            self.event.set()
+def main():
+    import sys
 
-        return data, flag
+    if len(sys.argv) < 2:
+        print('Usage: python {} music.wav'.format(sys.argv[0]))
+        sys.exit(1)
+
+    player = Player()
+    player.play(sys.argv[1], spectrum=True)
+
+
+if __name__ == '__main__':
+    main()
