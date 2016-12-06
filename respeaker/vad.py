e1f7019d6597ee181c811e3a8cbe7a5690f625e9
Yihui Xiong <yihui_xiong@163.com>
2016-12-06 17:09:01 +0800

    add level option, default is level 0

--- respeaker/vad.py
+++ respeaker/vad.py
@@ -1,3 +1,20 @@
+"""
+ ReSpeaker Python Library
+ Copyright (c) 2016 Seeed Technology Limited.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+"""
+
 import collections
 import sys
 
@@ -5,13 +22,19 @@ import webrtcvad
 
 
 class WebRTCVAD:
-    def __init__(self, sample_rate=16000):
+    def __init__(self, sample_rate=16000, level=0):
+        """
+
+        Args:
+            sample_rate: audio sample rate
+            level: between 0 and 3. 0 is the least aggressive about filtering out non-speech, 3 is the most aggressive.
+        """
         self.sample_rate = sample_rate
 
         self.frame_ms = 30
         self.frame_bytes = 2 * self.frame_ms * self.sample_rate / 1000   # S16_LE, 2 bytes width
 
-        self.vad = webrtcvad.Vad(3)
+        self.vad = webrtcvad.Vad(level)
         self.active = False
         self.data = ''
         self.history = collections.deque(maxlen=128)
